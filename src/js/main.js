55a3f567e8b9b6d5a4e6cc401cfd7a9935afa45b
th3eesome <610870693@qq.com>
2018-03-17 20:38:59 +0800

    new version

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,12 @@
 let map = {};
 let markers = [];
 const keyWords = [
-    '披萨', '巧克力', '烤肉', '火锅', '自助餐'
+    '披萨', '巧克力', '烤肉', '火锅', '自助餐', '全部'
 ];
 const bellTower = [108.947001,34.259458];
 
 
 window.init = function () {
-
-
     map = new AMap.Map('ali_map', {
         center: bellTower,
         zoom: 18
@@ -19,7 +17,7 @@ window.init = function () {
 
     map.setFitView();
 };
-
+/*地图初始化*/
 
 function viewModel() {
 
@@ -28,56 +26,70 @@ function viewModel() {
     const self = this;
 
     self.searchKey = ko.observable('钟楼附近美食');
+    self.displayKey = ko.observable('钟楼附近美食');
     self.startArray = ko.observableArray();
     self.startKey = '肯德基';
-    self.count = ko.observable(0);
-    self.center = [108.947001,34.259458];
-    self.city = 610100000000;
+
     const infoWindow = new AMap.InfoWindow();
 
-    self.increment = function () {
-        self.count(self.count() + 1);
-    };
 
     self.showWindow = function(e) {
         console.log(e);
-        infoWindow.setContent(e.target.content);
+        infoWindow.setContent(e.target.content.join('<br>'));
         infoWindow.open(map, e.target.getPosition());
     };
+    /*用于展示商户的信息*/
 
     self.gererateMarker = function () {
         map.setZoomAndCenter(18, bellTower);
-        console.log(self.startArray());
         self.startArray().forEach(d => {
             const newMarker = new AMap.Marker({
                 map: map,
                 position: d.location.split(','),
                 title: d.name,
-                id: d.id
             });
-
-            newMarker.content = '这里是' + d.name;
+            newMarker.content = ['名称:'+ d.name, '地址:' + d.address, '电话:' + d.tel];
             newMarker.on('click', self.showWindow);
             newMarker.setAnimation('AMAP_ANIMATION_DROP');
             markers.push(newMarker);
-
         });
     };
+    /*为当前api返回的所有商户生成marker*/
 
-    self.hideMarker = function (data) {
+    self.hideMarker = function (data, index) {
+       // console.log(index());
+        if (markers.length > 0) {
+            for (let i = 0; i < markers.length; i++) {
+                if (i === index())
+                {
+                    markers[i].setMap(map);
+                    markers[i].setAnimation('AMAP_ANIMATION_DROP');
+                }  else {
+                    markers[i].setMap(null);
+                }
+            }
+        }
         map.setZoomAndCenter(18, data.location.split(','));
     };
+    /*仅显示选中的marker，并高亮显示对应地点*/
 
     self.clearMarker = function () {
         markers.forEach(marker => {
             marker.setMap(null);
         })
     };
+    /*清楚当前所有marker*/
 
     self.searchCate = function (data) {
         console.log(data);
        // self.searchKey = data;
-        self.searchKey(data);
+        if(data === '全部') {
+            self.searchKey('披萨|巧克力|烤肉|火锅|自助餐');
+            self.displayKey('钟楼附近美食');
+        } else {
+            self.searchKey(data);
+            self.displayKey(data);
+        }
         $.ajax({
             url: url,
             type: 'GET',
@@ -109,9 +121,11 @@ function viewModel() {
             },
             error: function (data) {
                 console.log(data);
+                window.alert(data.message);
             }
         });
     };
+    /*根据指定类别，访问api，获得相关的美食商户*/
 
     $.ajax({
         url: url,
@@ -141,8 +155,7 @@ function viewModel() {
             console.log(data);
         }
     });
-
-
+    /*默认加载各种分类的美食地点*/
 }
 
 ko.applyBindings(new viewModel());
